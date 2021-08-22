2898d89226e12c98a0a0e7cdd4fe3dfd3d2f97e5
IT20626488 <88665549+IT20626488@users.noreply.github.com>
2021-08-22 10:31:33 +0530

    Create maian.java

---
+++ maian.java
@@ -0,0 +1,9 @@
+public class hello{
+ public static void main(String[] args){
+System.out.println("hello world");
+System.out.println("hello wasana");
+System.out.println("hello pubudu");
+System.out.println("hello w\n its good \n to see you");
+
+}
+}
